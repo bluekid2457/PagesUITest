ffddff738573c573de396c05ef8cd5398414e693
Achint <bluekid2457@gmail.com>
2024-07-01 12:27:28 -0700

    lotta stuff

---
+++ create_json.py
@@ -0,0 +1,20 @@
+import json
+import os
+from datetime import datetime
+
+# Create the directory if it does not exist
+directory = "APIData"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Create the JSON file with the current date
+date_str = datetime.now().strftime("%Y-%m-%d")
+file_name = f"report_{date_str}.json"
+file_path = os.path.join(directory, file_name)
+
+# Write the data to the JSON file
+data = {"x": 123}
+with open(file_path, 'w') as json_file:
+    json.dump(data, json_file)
+
+print(f"Created {file_path}")
