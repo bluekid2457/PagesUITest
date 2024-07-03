e3f5d09c3027ef1985f76cb34540843482a3f222
Achint <bluekid2457@gmail.com>
2024-07-03 15:37:23 -0700

    changed requirements

--- Scraping/random testing.py
+++ Scraping/random testing.py
@@ -1,7 +1,9 @@
 import glob
 import os
-print(os.curdir)
-list_of_files = glob.glob('C:/Users/ajua/Achint_Local_Coding/Scraping/hi/*') # * means all if need specific format then *.csv
-print(list_of_files)
-latest_file = max(list_of_files, key=os.path.getctime)
-print(latest_file)
+import bs4
+print(bs4.__version__)
+# print(os.curdir)
+# list_of_files = glob.glob('C:/Users/ajua/Achint_Local_Coding/Scraping/hi/*') # * means all if need specific format then *.csv
+# print(list_of_files)
+# latest_file = max(list_of_files, key=os.path.getctime)
+# print(latest_file)
